862fb4084ab82110c4520996d88449be6c9fc7ee
Tanishq Singh <tanishqsingh640@gmail.com>
2022-10-06 02:41:45 +0530

    :hammer: new: `facicon.ico`, `logo.svg` and formatted code (#3)

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,9 +1,14 @@
-import Counter from "../islands/Counter.tsx";
-
 export default function Home() {
-  return (
-    <div class="p-4 h-screen w-full flex justify-center items-center">
-    	<a href="https://github.com/tanishq-singh-2301" target="_blank" rel="noopener" class="text-base font-semibold hover:underline underline-offset-2">Tanishq Singh</a>
-	</div>
-  );
+	return (
+		<div class="h-screen w-full flex justify-center items-center">
+			<a
+				href="https://github.com/tanishq-singh-2301"
+				target="_blank"
+				rel="noopener"
+				class="text-base font-semibold hover:underline underline-offset-2"
+			>
+				Tanishq Singh
+			</a>
+		</div>
+	);
 }
