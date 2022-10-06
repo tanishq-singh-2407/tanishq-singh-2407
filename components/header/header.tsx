a7cc611a221e220577f86d98c7b6f1d0912539af
Tanishq Singh <tanishqsingh640@gmail.com>
2022-10-06 12:02:01 +0530

    :tada: add: `header` with custom fonts (#5)

---
+++ components/header/header.tsx
@@ -0,0 +1,19 @@
+import { JSX } from "preact";
+import { Link } from "./index.ts";
+
+export function Header(props: JSX.HTMLAttributes<HTMLElement>) {
+	return (
+		<header
+			{...props}
+			class="h-36 sm:h-44 w-full px-10 sm:px-20 md:px-28 lg:px-32 flex justify-start items-center fixed top-0"
+		>
+			<Link href="/home" name="Home" />
+			<Link href="/resume" name="Resume" />
+
+			<div class="w-full" />
+
+			<Link href="/projects" name="Projects" />
+			<Link href="/contact" name="Contact" />
+		</header>
+	);
+}
--- twind.config.ts
+++ twind.config.ts
@@ -2,4 +2,15 @@ import { Options } from "$fresh/plugins/twind.ts";
 
 export default {
 	selfURL: import.meta.url,
+	theme: {
+		extend: {
+			fontFamily: {
+				"awesome": "'Open Sans', sans-serif",
+			},
+		},
+	},
+	preflight: {
+		"@import":
+			"url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap')",
+	},
 } as Options;
