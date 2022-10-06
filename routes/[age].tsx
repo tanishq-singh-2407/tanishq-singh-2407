a9e59abb99791460b559a92e51149b33e5fd0e4c
Tanishq Singh <tanishqsingh640@gmail.com>
2022-10-06 13:45:38 +0530

    :truck: refactor: `ico` and `title` (#9)

--- routes/[age].tsx
+++ routes/[age].tsx
@@ -1,8 +1,9 @@
 import { Header } from "$components/header/index.ts";
 import { Footer } from "$components/footer/index.ts";
 import { Head } from "$fresh/runtime.ts";
+import { PageProps } from "$fresh/server.ts";
 
-export default function Home() {
+export default function Home({ params }: PageProps) {
 	return (
 		<div class="h-screen w-full text-[#333333] px-10 sm:px-20 md:px-28 lg:px-32 flex justify-between items-center flex-col">
 			<Head>
@@ -12,14 +13,11 @@ export default function Home() {
 			<Header />
 
 			<main class="w-full flex justify-center items-center">
-				<a
-					href="https://github.com/tanishq-singh-2301"
-					target="_blank"
-					rel="noopener"
-					class="text-5xl hover:text-black duration-200 font-bold text-center"
-				>
-					TANISHQ SINGH
-				</a>
+				<span class="text-3xl hover:text-black duration-200 font-bold text-center">
+					{params.age === "17"
+						? `My age is ${params.age} 🦕`
+						: "Changing that param doesn't change the fact, I'm still 17. 🤥"}
+				</span>
 			</main>
 
 			<Footer />
