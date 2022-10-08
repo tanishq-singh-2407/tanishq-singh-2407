9d816d8710b43ca89030acdc069db290c18504ba
Tanishq Singh <tanishqsingh640@gmail.com>
2022-10-09 01:09:32 +0530

    :sparkles: fix: resume task

--- components/resume/task.tsx
+++ components/resume/task.tsx
@@ -1,13 +1,13 @@
 import { JSX } from "preact";
 
 export type Task = {
-	date: string;
-	links: {
+	date?: string;
+	links?: {
 		href: string;
 		name: string;
 	}[];
-	title: string;
-	short_description: string;
+	title?: string;
+	short_description?: string;
 	point_description: {
 		link?: {
 			name: string;
@@ -25,26 +25,26 @@ type Props =
 
 export function Task(props: Props) {
 	return (
-		<div {...props} class="w-full flex flex-col lg:flex-row items-start mb-10" >
+		<div {...props} class="w-full flex flex-col lg:flex-row items-start mb-10">
+
 			<div class="w-full max-w-[16rem] flex lg:flex-col justify-start lg:justify-end lg:pr-10 items-center lg:items-end">
-				<span class="text-[#0009] text-base lg:text-[1.1rem] mb-1">
-					{props.task.date}
-				</span>
-				<div class="hidden lg:flex flex-col justify-end items-center">
+				{props.task.title && <span class="text-[#0009] text-base lg:text-[1.1rem] mb-1">{props.task.date}</span>}
+
+				{props.task.links?.length && <div class="hidden lg:flex flex-col justify-start items-end">
 					{props.task.links.map(({ href, name }) => <a href={href} class="text-blue-900 font-medium" key={href} >{name}</a>)}
-				</div>
+				</div>}
 			</div>
+
 			<div class="w-full flex flex-col justify-start items-start">
-				<span class="font-bold text-lg lg:text-[1.2rem] border-b-1 border-[#594f4f] border-dotted">
-					{props.task.title}
-				</span>
-				<div class="w-full lg:hidden flex justify-start items-center gap-2">
+				{props.task.title && <span class="font-bold text-lg lg:text-[1.2rem] border-b-1 border-[#594f4f] border-dotted">{props.task.title}</span>}
+				
+				{props.task.links?.length && <div class="w-full lg:hidden flex justify-start items-center gap-2">
 					{props.task.links.map(({ href, name }) => <a href={href} class="text-blue-900 font-medium" key={href} >{name}</a>)}
-				</div>
-				<span class="font-medium text-base lg:text-[1rem] mt-1 mb-2 text-[#030303]">
-					{props.task.short_description}
-				</span>
-				<ul class="list-disc ml-5 flex flex-col gap-2 text-[0.95rem]">
+				</div>}
+				
+				{props.task.short_description && <span class="font-medium text-base lg:text-[1rem] mt-1 text-[#030303]">{props.task.short_description}</span>}
+				
+				<ul class="list-disc ml-5 flex flex-col gap-2 text-[0.95rem] mt-2">
 					{props.task.point_description.map(({ lorem, link },index) => 
 						<li key={index}>
 							{link && <a href={link.href} class="border-b-1 border-black">{link.name}</a>}
--- routes/resume/index.tsx
+++ routes/resume/index.tsx
@@ -7,15 +7,22 @@ export default function Resume() {
 			<main class="flex-1 w-full flex flex-col justify-start items-start px-3">
 				<div class="w-full flex flex-col justify-center items-start mt-3">
 					<div class="flex">
-						<span class="text-3xl sm:text-4xl md:text-5xl lg:text-6xl hover:text-black duration-200 font-montserrat font-semibold text-center pr-2">Tanishq</span>
-						<span class="text-3xl sm:text-4xl md:text-5xl lg:text-6xl hover:text-black duration-200 font-montserrat font-thin text-center">Singh</span>
+						<span class="text-3xl sm:text-4xl md:text-5xl lg:text-6xl hover:text-black duration-200 font-montserrat font-semibold text-center pr-2">
+							Tanishq
+						</span>
+						<span class="text-3xl sm:text-4xl md:text-5xl lg:text-6xl hover:text-black duration-200 font-montserrat font-thin text-center">
+							Singh
+						</span>
 					</div>
-					<span class="text-xs md:text-sm font-semibold text-[#999] ml-1 md:ml-2 md:mt-1 border-b-1 border-[#999] border-dotted">fullstack & devops developer</span>
+					<span class="text-xs md:text-sm font-semibold text-[#999] ml-1 md:ml-2 md:mt-1 border-b-1 border-[#999] border-dotted">
+						fullstack & devops developer
+					</span>
 				</div>
 				<section class="w-full flex flex-col max-w-4xl">
 					<article class="w-full flex flex-col mt-20">
-						<span class="text-xl sm:text-2xl font-bold text-blue-600 tracking-wide mb-4 lg:ml-64">Projects</span>
-						{/* <Task /> */}
+						<span class="text-xl sm:text-2xl font-bold text-blue-600 tracking-wide mb-4 lg:ml-64">
+							{/* Projects */}
+						</span>
 					</article>
 				</section>
 			</main>
