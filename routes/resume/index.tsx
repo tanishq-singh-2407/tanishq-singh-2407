e589faa620a1f45c2f85ba0b1cd8ae8b1d4e646e
Tanishq Singh <tanishqsingh640@gmail.com>
2022-10-08 15:38:47 +0530

    :hankey: fix: red bg in resume

--- routes/resume/index.tsx
+++ routes/resume/index.tsx
@@ -3,45 +3,8 @@ import { App } from "$components/layouts/_app.tsx";
 export default function Resume() {
 	return (
 		<App header footer>
-			<main class="w-full flex flex-col bg-red-700 justify-center items-center">
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">1</span>
+			<main class="w-full flex flex-col justify-center items-center">
 				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">Resume</span>
-				<span class="text-5xl hover:text-black duration-200 font-bold text-center">10</span>
 			</main>
 		</App>
 	);
