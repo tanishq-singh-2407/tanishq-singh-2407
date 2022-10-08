1d15f71e4addc959ec4da311a374869c93555055
Tanishq Singh <tanishqsingh640@gmail.com>
2022-10-08 20:15:55 +0530

    :hankey: fix: resume name made responsive (#24)

--- routes/resume/index.tsx
+++ routes/resume/index.tsx
@@ -6,10 +6,10 @@ export default function Resume() {
 			<main class="flex-1 w-full flex flex-col justify-start items-center px-3">
 				<div class="w-full flex flex-col justify-center items-start my-10">
 					<div class="flex">
-						<span class="text-3xl hover:text-black duration-200 font-montserrat font-semibold text-center pr-2">Tanishq</span>
-						<span class="text-3xl hover:text-black duration-200 font-montserrat font-thin text-center">Singh</span>
+						<span class="text-3xl md:text-5xl hover:text-black duration-200 font-montserrat font-semibold text-center pr-2">Tanishq</span>
+						<span class="text-3xl md:text-5xl hover:text-black duration-200 font-montserrat font-thin text-center">Singh</span>
 					</div>
-					<span class="text-xs font-semibold text-[#999] ml-1 border-b-1 border-[#999] border-dotted">fullstack & devops developer</span>
+					<span class="text-xs md:text-sm font-semibold text-[#999] ml-1 md:ml-2 md:mt-1 border-b-1 border-[#999] border-dotted">fullstack & devops developer</span>
 				</div>
 			</main>
 		</App>
