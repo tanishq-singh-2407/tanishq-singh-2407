c918fccf623a3c6f5c07c8fd0bcd61aa50a934e0
Tanishq Singh <tanishqsingh640@gmail.com>
2022-10-22 03:20:47 +0530

    :fire: feat: add noise in bg `_app.tsx` (#36)

--- components/layouts/_app.tsx
+++ components/layouts/_app.tsx
@@ -1,5 +1,6 @@
 import { Header } from "$components/header/header.tsx";
 import { Footer } from "$components/footer/footer.tsx";
+import { Noise } from "$components/noise/noise.tsx";
 import { Head } from "$fresh/runtime.ts";
 import { JSX } from "preact/jsx-runtime";
 
@@ -11,10 +12,7 @@ type Props = JSX.HTMLAttributes<HTMLDivElement> & {
 
 export function App(props: Props) {
 	return (
-		<div
-			{...props}
-			class="min-h-full w-full px-10 sm:px-20 md:px-28 lg:px-32 flex justify-between items-center flex-col"
-		>
+		<div {...props} class="min-h-full w-full px-10 sm:px-20 md:px-28 lg:px-32 flex justify-between items-center flex-col">
 			<Head>
 				<title>Tanishq Singh</title>
 			</Head>
@@ -22,6 +20,8 @@ export function App(props: Props) {
 			{props.header && <Header />}
 
 			{props.children}
+
+			<Noise />
 
 			{props.footer && <Footer />}
 		</div>
