3ce5c940ec198890ad897d572894e2a513d8c343
Tanishq Singh <tanishqsingh640@gmail.com>
2022-12-21 09:15:29 +0000

    :wrench: test: on push NaN issue

--- game/index.ts
+++ game/index.ts
@@ -1,6 +1,6 @@
 import { parse } from "https://deno.land/std@0.158.0/flags/mod.ts";
 const { username, guessed_number } = parse(Deno.args);
 
-console.log({ username, guessed_number })
+// console.log({ username, guessed_number })
 
 // deno run game/index.ts --username=tanishq-singh-2301 --guessed_number=10
