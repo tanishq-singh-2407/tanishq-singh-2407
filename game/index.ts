ad671ac1937e17b4df893f60f1631fd79dba497e
Tanishq Singh <tanishqsingh640@gmail.com>
2022-12-21 09:51:09 +0000

    :hammer: beta: game test 1

--- game/index.ts
+++ game/index.ts
@@ -1,9 +1,10 @@
 import { parse } from "https://deno.land/std@0.158.0/flags/mod.ts";
-const { username, guessed_number } = parse(Deno.args);
+const { guessed_number } = parse(Deno.args);
 
-// console.log({ username, guessed_number })
+if (Math.floor(Math.random() * 9) === guessed_number){
+    console.log("got-it");
+}
 
-console.log("missed-it");
-
-
-// deno run game/index.ts --username=tanishq-singh-2301 --guessed_number=10
+else {
+    console.log("missed-it");
+}
