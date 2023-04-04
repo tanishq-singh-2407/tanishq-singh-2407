5e7515cc51ed18e1cb5614be18b04cdd5fbcb53f
Tanishq Singh <tanishqsingh640@gmail.com>
2023-04-04 18:37:26 +0000

    :hammer: [feat]: `profile-count` add folio

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -1,12 +1,12 @@
 import { MiddlewareHandlerContext } from "$fresh/server.ts";
+import connect from "$db/connect.ts";
 
-interface State {
-    data: string;
-}
+export const handler = async (_req: Request, ctx: MiddlewareHandlerContext) => {
+    const client = connect();
+    if (typeof client !== "string")
+        await client.queryObject<{ value: number }>("UPDATE stats SET value = VALUE + 1 where id ='dd3f04a5-4b5f-4981-857c-49539ae5d808'");
 
-export const handler = async (_req: Request, ctx: MiddlewareHandlerContext<State>) => {
     const resp = await ctx.next();
-    resp.headers.set("server", "fresh server");
 
     return resp;
 }
