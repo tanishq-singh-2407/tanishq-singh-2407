cac86cc3f6a026e929b30451d16321159ca85434
Your Name <you@example.com>
2024-07-22 06:40:40 +0530

    :broom: cleaned everything

--- routes/leetcode.tsx
+++
@@ -1,7 +0,0 @@
-import { Handlers } from "$fresh/server.ts";
-
-export const handler: Handlers = {
-    GET() {
-        return Response.redirect("https://leetcode.com/tanishq-singh-2407");
-    },
-};
